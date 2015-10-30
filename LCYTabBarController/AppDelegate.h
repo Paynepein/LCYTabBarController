8508505fd47df95e6e6a6733ec11591a3ea9b9f6
lichangyuan <changyuan.li@cloudbaoxiao.com>
2015-10-30 18:50:54 +0800

    Initial Commit

---
+++ LCYTabBarController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LCYTabBarController
+//
+//  Created by lichangyuan on 15/10/30.
+//  Copyright (c) 2015年 lichangyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
